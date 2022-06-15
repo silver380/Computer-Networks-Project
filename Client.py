caf20533389ba6747091a90478a3ec52c13624bd
silver380 <r.rastegari380@outlook.com>
2022-06-15 14:33:09 +0430

    Sending file is possible now

--- Client.py
+++ Client.py
@@ -1,25 +1,53 @@
 import socket
 import tqdm
 import os
+import pandas
 ClientSocket = socket.socket()
 host = '127.0.0.1'
-port = 1233
-SEPARATOR = "<SEPARATOR>"
+port = 1223
 BUFFER_SIZE = 4096  # send 4096 bytes each time step
+SEPARATOR = "<SEPARATOR>"
 print('Waiting for connection')
 try:
+    print(f"[+] Connecting to {host}:{port}")
     ClientSocket.connect((host, port))
+    print("[+] Connected.")
+    # print(ClientSocket.recv(BUFFER_SIZE).decode())
     client_name = input("Enter the faculty name: ")
     ClientSocket.send(str.encode(client_name))
     Response = ClientSocket.recv(1024)
     print(f"{client_name} is connected to the server", client_name)
-    print("enter the path of your faculty csv:")
-
-
 except socket.error as e:
     print(str(e))
 
-Response = ClientSocket.recv(1024)
+while True:
+    try:
+        file_name = input("enter the path and name of your faculty csv:\n")
+        file_size = os.path.getsize(file_name)
+        ClientSocket.send(f"{file_size}".encode())
+        progress = tqdm.tqdm(range(file_size), f"Sending {file_name}", unit="B", unit_scale=True, unit_divisor=1024)
+        # ClientSocket.send(f"{file_name}".encode())
+        total_send = 0
+        with open(file_name, "rb") as f:
+            while True:
+                # read the bytes from the file
+                bytes_read = f.read(BUFFER_SIZE)
+                total_send += len(bytes_read)
+                ClientSocket.sendall(bytes_read)
+                progress.update(len(bytes_read))
+                if total_send == file_size:
+                    # file transmitting is done
+                    break
+                # update the progress bar
+
+            massage = ClientSocket.recv(BUFFER_SIZE).decode()
+            print(massage)
+
+    except os.error as e:
+        print(str(e))
+        continue
+    # break
+
 while True:
     Input = input('Say Something: ')
     ClientSocket.send(str.encode(Input))
--- Server.py
+++ Server.py
@@ -1,34 +1,65 @@
+import os.path
 import socket
 import subprocess
 import sqlite3
+import tqdm
 from _thread import *
 
 ServerSocket = socket.socket()
 host = '127.0.0.1'
-port = 1233
+port = 1223
+BUFFER_SIZE = 4096
+SEPARATOR = "<SEPARATOR>"
 ThreadCount = 0
 
-try:
-    ServerSocket.bind((host, port))
-except socket.error as e:
-    print(str(e))
+while True:
+    try:
+        ServerSocket.bind((host, port))
+    except socket.error as e:
+        print(str(e))
+        continue
+    break
 
 # Creating Clients
 client_number = int(input("Enter the number of Clients:"))
-print('Waiting for a Connection..')
+print(f"[*] Listening as {host}:{port}")
 ServerSocket.listen(client_number)
 for i in range(client_number):
-    subprocess.call(["gnome-terminal", "-x", "sh", "-c", "python3 ./Client.py"])
+    subprocess.call(["gnome-terminal", "--", "sh", "-c", "python3 ./Client.py"])
 
 
 def threaded_client(connection):
-    connection.send(str.encode('Welcome to the Server'))
-    while True:
-        data = connection.recv(2048)
-        reply = 'Server Says: ' + data.decode('utf-8')
-        if not data:
-            break
-        connection.sendall(str.encode(reply))
+    try:
+        connection.send(str.encode('Welcome to the Server'))
+        client_name = connection.recv(BUFFER_SIZE).decode()
+        print(client_name)
+        getting_file = True
+        while getting_file:
+            data = connection.recv(BUFFER_SIZE).decode()
+            if not data:
+                break
+            file_size = int(data)
+            print("size is", file_size)
+            file_name = os.path.basename(f"./{client_name}.csv")
+            progress = tqdm.tqdm(range(file_size), f"Saving in {file_name}", unit="B", unit_scale=True,
+                                 unit_divisor=1024)
+            total_rcv = 0
+            with open(file_name, "wb") as f:
+                while True:
+                    # read 1024 bytes from the socket (receive)
+                    bytes_read = connection.recv(BUFFER_SIZE)
+                    total_rcv += len(bytes_read)
+                    f.write(bytes_read)
+                    progress.update(len(bytes_read))
+                    if total_rcv == file_size:
+                        # nothing is received
+                        # file transmitting is done
+                        getting_file = False
+                        break
+                    # write to the file the bytes we just received
+            connection.send(str.encode("saved successfully"))
+    except socket.error as er:
+        print(str(er))
     connection.close()
 
 
