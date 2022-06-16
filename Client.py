d2948e8b1910a55fd5eee5aa058ae34ab3050462
silver380 <r.rastegari380@outlook.com>
2022-06-16 15:01:05 +0430

    Average and Sort added, code refactored

--- Client.py
+++ Client.py
@@ -1,54 +1,108 @@
 import socket
 import tqdm
 import os
-import pandas
+import sys
+
 ClientSocket = socket.socket()
 host = '127.0.0.1'
 port = 5000
-BUFFER_SIZE = 10000  # send 5000 bytes each time step
-print('Waiting for connection')
+BUFFER_SIZE = 10000
 
-try:
-    print(f"[+] Connecting to {host}:{port}")
-    ClientSocket.connect((host, port))
-    print("[+] Connected.")
-    client_name = input("Enter the faculty name: ")
-    ClientSocket.send(str.encode(client_name))
-    print(f"{client_name} is connected to the server", client_name)
-except socket.error as e:
-    print(str(e))
 
-while True:
+def send_file_info():
+    while True:
+        try:
+            file_name = input("enter the path and name of your faculty csv:\n")
+            file_size = os.path.getsize(file_name)
+            ClientSocket.send(f"{file_size}".encode())
+            progress = tqdm.tqdm(range(file_size), f"Sending {file_name}", mininterval=0.000000001,
+                                 maxinterval=0.000000001,
+                                 unit="B", unit_scale=True, unit_divisor=2048, colour='blue', delay=0)
+            total_send = 0
+            with open(file_name, "rb") as f:
+                while True:
+                    # read the bytes from the file
+                    bytes_read = f.read(BUFFER_SIZE)
+                    total_send += len(bytes_read)
+                    progress.update(len(bytes_read))
+                    ClientSocket.sendall(bytes_read)
+                    if total_send >= file_size:
+                        # file transmitting is done
+                        break
+            massage = ClientSocket.recv(BUFFER_SIZE).decode()
+            print(massage)
+        except os.error as er:
+            print(str(er))
+            continue
+        break
+
+
+def send_command():
+    while True:
+        os.system('clear')
+        command = input("Enter your command or enter help to see available commands:\n")
+        os.system('clear')
+        if command == "help":
+            print("you can use these commands to interact with the server:\n")
+            print("[*]Done: will close the connection\n")
+            print("[*]Average: See the average point of your faculty\n")
+            print("[*]Sort: See the list of your Students, sorted by their average\n")
+            print("[*]Max: See the first name and the last name of the student with the highest average\n")
+            print("[*]Min: See the first name and the last name of the student with the lowest average\n")
+            print("[*]Insert: add a new Student\n")
+            input("Press enter to go back")
+        if command == 'Average':
+            ClientSocket.send(command.encode())
+            data = ClientSocket.recv(100)
+            print(f"{client_name} Students Average is: ", data.decode())
+            input("press Enter to go back to main menu")
+        elif command == 'Sort':
+            ClientSocket.send(command.encode())
+            data = ClientSocket.recv(BUFFER_SIZE).decode()
+            sorted_file_size = int(data)
+            sorted_file_name = f"./Client_CSVs/Sorted_{client_name}.csv"
+            progress = tqdm.tqdm(range(sorted_file_size), f"Saving in {sorted_file_name}", mininterval=0.00000001,
+                                 maxinterval=0.00000001,
+                                 unit="B", unit_scale=True, unit_divisor=2048, colour='green')
+            total_rcv = 0
+            with open(sorted_file_name, "wb") as f:
+                while True:
+                    bytes_read = ClientSocket.recv(BUFFER_SIZE)
+                    total_rcv += len(bytes_read)
+                    progress.update(len(bytes_read))
+                    f.write(bytes_read)
+                    if total_rcv >= sorted_file_size:
+                        break
+            print(f"you can see the result in {sorted_file_name}")
+            input("press Enter to go back to main menu")
+
+
+
+
+# while True:
+#     Input = input('Say Something: ')
+#     ClientSocket.send(str.encode(Input))
+#     Response = ClientSocket.recv(1024)
+#     print(Response.decode('utf-8'))
+#
+# ClientheSocket.close()
+
+if __name__ == '__main__':
+    print("Welcome")
+    # connecting to server
     try:
-        file_name = input("enter the path and name of your faculty csv:\n")
-        file_size = os.path.getsize(file_name)
-        ClientSocket.send(f"{file_size}".encode())
-        progress = tqdm.tqdm(range(file_size), f"Sending {file_name}",mininterval=0.000000001, maxinterval=0.000000001, unit="B", unit_scale=True,
-                             unit_divisor=2048, colour='blue')
-        total_send = 0
-        with open(file_name, "rb") as f:
-            while True:
-                # read the bytes from the file
-                bytes_read = f.read(BUFFER_SIZE)
-                total_send += len(bytes_read)
-                progress.update(len(bytes_read))
-                ClientSocket.sendall(bytes_read)
-                if total_send >= file_size:
-                    print("ok")
-                    # file transmitting is done
-                    break
-        massage = ClientSocket.recv(BUFFER_SIZE).decode()
-        print(massage)
-
-    except os.error as e:
+        print(f"[+] Connecting to {host}:{port}")
+        ClientSocket.connect((host, port))
+        print("[+] Connected.")
+        client_name = input("Enter the faculty name: ")
+        ClientSocket.send(str.encode(client_name))
+        is_exist = int(ClientSocket.recv(4).decode())
+    except socket.error as e:
         print(str(e))
-        continue
-    break
-
-while True:
-    Input = input('Say Something: ')
-    ClientSocket.send(str.encode(Input))
-    Response = ClientSocket.recv(1024)
-    print(Response.decode('utf-8'))
-
-ClientheSocket.close()
+        sys.exit()
+    if not is_exist:
+        send_file_info()
+        os.system('clear')
+        send_command()
+    else:
+        send_command()
--- Server.py
+++ Server.py
@@ -2,6 +2,7 @@ import os.path
 import socket
 import subprocess
 import tqdm
+import pandas as pd
 from _thread import *
 
 ServerSocket = socket.socket()
@@ -10,46 +11,74 @@ port = 5000
 BUFFER_SIZE = 1000
 ThreadCount = 0
 
-while True:
-    try:
-        ServerSocket.bind((host, port))
-    except socket.error as e:
-        print(str(e))
-        continue
-    break
 
 # Creating Clients
-client_number = int(input("Enter the number of Clients:"))
-print(f"[*] Listening as {host}:{port}")
-ServerSocket.listen(client_number)
-for i in range(client_number):
-    subprocess.call(["gnome-terminal", "--", "sh", "-c", "python3 ./Client.py"])
+
+def creating_clients():
+    client_number = int(input("Enter the number of Clients:"))
+    print(f"[*] Listening as {host}:{port}")
+    ServerSocket.listen(client_number)
+    for i in range(client_number):
+        subprocess.call(["gnome-terminal", "--", "sh", "-c", "python3 ./Client.py"])
 
 
 def threaded_client(connection):
     try:
         client_name = connection.recv(1000).decode()
-        print(client_name)
-        data = connection.recv(BUFFER_SIZE).decode()
-        file_size = int(data)
-        print("size is", file_size)
-        file_name = f"./{client_name}.csv"
-        progress = tqdm.tqdm(range(file_size), f"Saving in {file_name}",mininterval=0.00000001, maxinterval=0.00000001, unit="B", unit_scale=True,
-                             unit_divisor=2048, colour='blue')
-        total_rcv = 0
-        with open(file_name, "wb") as f:
-            while True:
-                bytes_read = connection.recv(BUFFER_SIZE)
-                total_rcv += len(bytes_read)
-                progress.update(len(bytes_read))
-                f.write(bytes_read)
-                if total_rcv >= file_size:
-                    break
-        connection.sendall(str.encode("saved successfully"))
-        while True:
+        if not os.path.exists(f"./Server_CSVs/{client_name}.csv"):
+            connection.send('0'.encode())
             data = connection.recv(BUFFER_SIZE).decode()
-            if not data:
-                break
+            file_size = int(data)
+            file_name = f"./Server_CSVs/{client_name}.csv"
+            progress = tqdm.tqdm(range(file_size), f"Saving in {file_name}", mininterval=0.00000001,
+                                 maxinterval=0.00000001,
+                                 unit="B", unit_scale=True, unit_divisor=2048, colour='blue')
+            total_rcv = 0
+            with open(file_name, "wb") as f:
+                while True:
+                    bytes_read = connection.recv(BUFFER_SIZE)
+                    total_rcv += len(bytes_read)
+                    progress.update(len(bytes_read))
+                    f.write(bytes_read)
+                    if total_rcv >= file_size:
+                        break
+            connection.sendall(str.encode("saved successfully"))
+        else:
+            connection.send('1'.encode())
+        client_df = pd.read_csv(f"./Server_CSVs/{client_name}.csv")
+        client_df['Average'] = client_df.iloc[:, 4:9].mean(axis=1)
+        while True:
+            command = connection.recv(BUFFER_SIZE).decode()
+            if command == 'Average':
+                average = client_df['Average'].mean()
+                connection.send(str(average).encode())
+            elif command == 'Sort':
+                sorted_df = client_df.copy().sort_values(by='Average', ascending=False)[['Second_ID', 'Average']]
+                sorted_df.to_csv(path_or_buf=f"./Server_CSVs/Sorted_{client_name}.csv",index=False)
+                try:
+                    sorted_file_name = f"./Server_CSVs/Sorted_{client_name}.csv"
+                    sorted_file_size = os.path.getsize(sorted_file_name)
+                    connection.send(f"{sorted_file_size}".encode())
+                    progress = tqdm.tqdm(range(sorted_file_size), f"Sending {sorted_file_name}", mininterval=0.000000001,
+                                         maxinterval=0.000000001,
+                                         unit="B", unit_scale=True, unit_divisor=2048, colour='green', delay=0)
+                    total_send = 0
+                    with open(sorted_file_name, "rb") as f:
+                        while True:
+                            # read the bytes from the file
+                            bytes_read = f.read(BUFFER_SIZE)
+                            total_send += len(bytes_read)
+                            progress.update(len(bytes_read))
+                            connection.sendall(bytes_read)
+                            if total_send >= sorted_file_size:
+                                # file transmitting is done
+                                break
+                    os.remove(sorted_file_name)
+                except os.error as er:
+                    print(str(er))
+                    continue
+
+
 
     except socket.error as er:
         print(str(er))
@@ -57,11 +86,19 @@ def threaded_client(connection):
     # connection.close()
 
 
-while True:
-    Client, address = ServerSocket.accept()
-    print('Connected to: ' + address[0] + ':' + str(address[1]))
-    start_new_thread(threaded_client, (Client,))
-    ThreadCount += 1
-    print('Thread Number: ' + str(ThreadCount))
-
-ServerSocket.close()
+if __name__ == '__main__':
+    while True:
+        try:
+            ServerSocket.bind((host, port))
+        except socket.error as e:
+            print(str(e))
+            continue
+        break
+    creating_clients()
+    while True:
+        Client, address = ServerSocket.accept()
+        print('Connected to: ' + address[0] + ':' + str(address[1]))
+        start_new_thread(threaded_client, (Client,))
+        ThreadCount += 1
+        print('Thread Number: ' + str(ThreadCount))
+    ServerSocket.close()
